0aa8a94f51b8b94a7312559885c565a9cc0bcb39
dnice <dnice_bo@Outlook.com>
2020-02-21 17:35:13 +0800

    fix formAccounSetting css

--- src/views/forms/FormAccountSetting/FormAccountSetting.vue
+++ src/views/forms/FormAccountSetting/FormAccountSetting.vue
@@ -11,6 +11,20 @@
           <div class="left-container">
             <div class="form-row">
               <div class="label">
+                <FormLabel>{{ $t('Set_seed_hash') }}</FormLabel>
+              </div>
+              <div class="value">
+                <div class="full-width-item-container">
+                  <p
+                    class="input-size">
+                    C6DAFB1CDDF22089C76E7130DB7B2919B9AD3AEF1CEEE44C2ABCF1252B4458D6
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="form-row">
+              <div class="label">
                 <FormLabel>{{ $t('Set_account_name') }}</FormLabel>
               </div>
               <div class="value">
@@ -35,7 +49,7 @@
                 </ValidationProvider>
               </div>
             </div>
-            <div class="form-rowr">
+            <div class="form-row">
               <div class="label">
                 <FormLabel>{{ $t('Set_password') }}</FormLabel>
               </div>
@@ -91,7 +105,7 @@
               </div>
             </div>
             <div class="form-row">
-              <div class="lable">
+              <div class="label">
                 <FormLabel>{{ $t('Set_password_hint') }}</FormLabel>
               </div>
               <div class="value">
@@ -129,24 +143,24 @@
       <div class="warm-tips-text">
         {{ $t('Set_account_description_tips2') }}
       </div>
+         
+      <div class="button-container mt-3">
+        <button
+          type="button"
+          class="button-style back-button" 
+          @click="$router.push({name: 'accounts.login'})"
+        >
+          {{ $t('Previous_step') }}
+        </button>
+        <button
+          type="submit"
+          class="button-style validation-button" 
+          @click="handleSubmit(submit)"
+        >
+          {{ $t('next') }}
+        </button>
+      </div>
     </div>
-   
-    <!--<div class="flex-container mt-3">
-      <button
-        type="button"
-        class="button-style back-button" 
-        @click="$router.push({name: 'accounts.login'})"
-      >
-        {{ $t('Previous_step') }}
-      </button>
-      <button
-        type="submit"
-        class="button-style validation-button" 
-        @click="handleSubmit(submit)"
-      >
-        {{ $t('next') }}
-      </button>
-    </div>-->
   </div>
 </template>
 
@@ -161,16 +175,16 @@ export default class FormAccountSetting extends FormAccountSettingTs {}
   display: inline-block;
   width: 100%;
   clear: both;
-  min-height: 71px;
+  min-height: 1rem;
   .form-row {
     width: 100%;
-    margin-top: 15px;
+    margin-top: 20px;
     display: flex;
   }
   .label {
     display: inline-block;
-    text-align: right;
     font-size: @normalFont;
+    text-align: right;
     width: 100%;
     margin-right: 25px;
     margin-top: 10px;
@@ -195,11 +209,19 @@ export default class FormAccountSetting extends FormAccountSettingTs {}
     color:rgba(51,51,51,1);
 }
 .right-container {
-    display: block;
-    position: absolute;
-    width: 5rem;
-    float: left;
-    left: 10.5rem;
-    top: 2.5rem;
-  }
+  display: block;
+  position: absolute;
+  width: 5rem;
+  float: left;
+  left: 10.5rem;
+  top: 2.5rem;
+}
+.button-container {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 200px;
+}
+.input-size{
+  width: 6.3rem;
+}
 </style>

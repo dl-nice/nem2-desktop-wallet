bdd7de720d913346cef7518d0bcf88e8a83076fd
dnice <dnice_bo@Outlook.com>
2020-02-24 22:33:21 +0800

    fix button css

--- src/views/pages/accounts/CreationPage/BackupMnemonic/BackupMnemonic.vue
+++ src/views/pages/accounts/CreationPage/BackupMnemonic/BackupMnemonic.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="backup-mnemonic-page-wrapper">
-       <div class="backup-mnemonic-container">
+            <div class="backup-mnemonic-container">
       <MnemonicDisplay :mnemonic-list="mnemonicList" />
     </div>
       <div class="right-tip-container">

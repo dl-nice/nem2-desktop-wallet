e8119b7d7cab8a12892f06ee3b6f4e9f0574fc9f
ghc <330125849@qq.com>
2020-02-25 11:45:56 +0800

    fix style

--- src/views/pages/accounts/CreationPage/BackupMnemonic/BackupMnemonic.vue
+++ src/views/pages/accounts/CreationPage/BackupMnemonic/BackupMnemonic.vue
@@ -1,30 +1,21 @@
 <template>
   <div>
     <div class="backup-mnemonic-page-wrapper">
-            <div class="backup-mnemonic-container">
-      <MnemonicDisplay :mnemonic-list="mnemonicList" />
-    </div>
+      <div class="backup-mnemonic-container">
+        <MnemonicDisplay :mnemonic-list="mnemonicList" />
+      </div>
       <div class="right-tip-container">
-        <RightTipDisplay 
-          :tip-content="tipContents"
-        />
+        <RightTipDisplay :tip-content="tipContents" />
       </div>
     </div>
     <div class="button-container">
-      <ButtonStep @click="$router.go(-1)">
-        上一步
-      </ButtonStep>
-      <ButtonStep :primary="true" @click="$router.push({name: $route.meta.nextPage})">
-        下一步
-      </ButtonStep>
+      <ButtonStep @click="$router.push({name: $route.meta.nextPage})">下一步</ButtonStep>
     </div>
   </div>
 </template>
 
 <script>
-import BackupMnemonicTs from './BackupMnemonicTs'
-import './BackupMnemonic.less'
-export default class BackupMnemonic extends BackupMnemonicTs {
-
-}
+import BackupMnemonicTs from "./BackupMnemonicTs";
+import "./BackupMnemonic.less";
+export default class BackupMnemonic extends BackupMnemonicTs {}
 </script>
